368e578946fc915a29ba7d7bd2cdb510c3182644
Julieta Cozza <julietacozza@Julietas-MBP.fibertel.com.ar>
2021-11-12 01:32:31 -0300

    last commit

--- app.js
+++ app.js
@@ -1,155 +1,167 @@
-const contenedorProductos = document.getElementById('contenedor-productos')
-const contenedorCarrito = document.getElementById('carrito-contenedor')
+const contenedorProductos = document.getElementById("contenedor-productos");
 
-const contadorCarrito = document.getElementById('contadorCarrito')
-const precioTotal = document.getElementById('precioTotal')
+var productos = [];
 
-const carrito = []
+$.ajax({
+  url: "./stock.json",
+  dataType: "json",
+  success: (response) => {
+    cargarStock(response, productos);
+  },
+});
 
-const mostrarProductos = (array) => {
-    contenedorProductos.innerHTML = ''
+function cargarStock(res, productos) {
+  res.forEach((producto) => {
+    var producto = new Producto(
+      producto.nombre,
+      producto.precio,
+      producto.id,
+      producto.img
+    );
+    productos.push(producto);
+  });
 
-    array.forEach((producto) => {
-        const div = document.createElement('div')
-        div.classList.add('producto')
-        div.innerHTML = `
-                    <img src=${producto.img} alt="">
-                    <h3>${producto.nombre}</h3>
-                    <p class="precioProducto">Precio: $${producto.precio}</p>
-                    <button onclick="agregarAlCarrito(${producto.id})" class="boton-agregar">Agregar <i class="fas fa-shopping-cart"></i></button>       
-        `
-        contenedorProductos.appendChild(div)
-    } )
+  console.log(productos);
+
+  mostrarProductos(productos);
 }
 
-mostrarProductos(productos);
+const mostrarProductos = (array) => {
+  contenedorProductos.innerHTML = "";
 
+  array.forEach((producto) => {
+    const div = document.createElement("div");
+    div.classList.add("producto");
+    div.innerHTML = `
+                    <img class="card-img" src=${producto.img} alt="">
+                    <h3 class= "card-title">${producto.nombre}</h3>
+                    <p class="precioProducto card-text">Precio: $${producto.precio}</p>
+                    <button onclick="agregarAlCarrito(${producto.id})" class="boton-agregar">Agregar <i class="fas fa-shopping-cart"></i></button>       
+        `;
+    contenedorProductos.appendChild(div);
+  });
+};
+
+const carrito = [];
 // agregar productos al carrito
 
 const agregarAlCarrito = (itemId) => {
+  const productoEnCarrito = carrito.find((prod) => prod.id === itemId);
 
-    const productoEnCarrito = carrito.find((prod) => prod.id === itemId)
+  if (productoEnCarrito) {
+    productoEnCarrito.cantidad++;
+  } else {
+    const producto = productos.find((prod) => prod.id === itemId);
 
-    if (productoEnCarrito) {
-        productoEnCarrito.cantidad++
-    } else {
+    carrito.push({
+      img: producto.img,
+      id: producto.id,
+      nombre: producto.nombre,
+      precio: producto.precio,
+      cantidad: 1,
+    });
+  }
 
-        const producto = productos.find((prod) => prod.id === itemId)
-
-        carrito.push({
-            img: producto.img,
-            id: producto.id,
-            nombre: producto.nombre,
-            precio: producto.precio,
-            cantidad: 1
-        })
-    }
-
-    actualizarCarrito()
-}
+  actualizarCarrito();
+};
 
 // actualizar carrito
 
+const contenedorCarrito = document.getElementById("contenedor-carrito");
+
+console.log(contenedorCarrito)
+
 const actualizarCarrito = () => {
-    contenedorCarrito.innerHTML = ""
+  contenedorCarrito.innerHTML = "";
 
-    carrito.forEach((prod) => {
-        const div = document.createElement('div')
-        div.classList.add('productoEnCarrito')
+  carrito.forEach((prod) => {
+    const div = document.createElement("div");
+    div.classList.add("productoEnCarrito");
 
-        div.innerHTML = `
+    div.innerHTML = `
                 <img src=${prod.img} width= "120px" alt="">
-                <h3 style="width: 50px; padding-right:1px">${prod.nombre}</h3>
+                <h5 style="width: 50px">${prod.nombre}</h5>
                 <p>Precio <br> $${prod.precio}</p>
                 <p>Cantidad <br>${prod.cantidad}</p>
                 <button onclick="eliminarProducto(${prod.id})" class="boton-eliminar"><i class="fas fa-trash-alt"></i></button>
-             `
+             `;
 
-        contenedorCarrito.appendChild(div)
-    })
+    contenedorCarrito.appendChild(div);
+  });
 
-    contadorCarrito.innerText = carrito.reduce((acc, prod) => acc += prod.cantidad, 0)
-    precioTotal.innerText = carrito.reduce((acc, prod) => acc += prod.precio * prod.cantidad, 0)
+  contadorCarrito.innerText = carrito.reduce(
+    (acc, prod) => (acc += prod.cantidad),
+    0
+  );
+  precioTotal.innerText = carrito.reduce(
+    (acc, prod) => (acc += prod.precio * prod.cantidad),
+    0
+  );
 
-    const contadorItems = JSON.stringify(contadorCarrito.innerText);
-    localStorage.setItem("contador items", contadorItems);
+  const totalProductos = JSON.stringify(precioTotal.innerText);
+  const contadorItems = JSON.stringify(contadorCarrito.innerText);
 
+  localStorage.setItem("cont productos en carrito ", contadorItems);
+  localStorage.setItem("productos en carrito", JSON.stringify(carrito));
+  localStorage.setItem("total productos", JSON.stringify(precioTotal.innerText));
 
-    const carritoJson = JSON.stringify(carrito.innerHTML);
-    localStorage.setItem('carrito', carrito)
-}
+  console.log(carrito);
+};
 
 // eliminar productos carrito
 
 const eliminarProducto = (itemId) => {
-    const producto = carrito.find((prod) => prod.id === itemId)
+  const producto = carrito.find((prod) => prod.id === itemId);
 
-    producto.cantidad--
+  producto.cantidad--;
 
-    if (producto.cantidad === 0) {
-        const index = carrito.indexOf(producto)
-        carrito.splice(index, 1)
-    }
-    actualizarCarrito()
-}
+  if (producto.cantidad === 0) {
+    const index = carrito.indexOf(producto);
+    carrito.splice(index, 1);
+  }
+  actualizarCarrito();
+};
 
-const contenedorModal = document.getElementsByClassName('modal-contenedor')[0]
-const botonAbrir = document.getElementById('boton-carrito')
-const botonCerrar = document.getElementById('carritoCerrar')
-const modalCarrito = document.getElementsByClassName('modal-carrito')[0]
+const contenedorModal = document.getElementsByClassName("modal-contenedor")[0];
+const botonAbrir = document.getElementById("boton-carrito");
+const botonCerrar = document.getElementById("carritoCerrar");
+const modalCarrito = document.getElementsByClassName("modal-carrito")[0];
 
-botonAbrir.addEventListener('click', ()=>{
-    contenedorModal.classList.add('modal-active')
-})
-botonCerrar.addEventListener('click', ()=>{
-    contenedorModal.classList.remove('modal-active')
-})
-contenedorModal.addEventListener('click', ()=>{
-    botonCerrar.click()
-})
-
-
-// log-in
-const abrirLogin = $(".login-abrir");
-const modalContainer = $(".modal-login-contenedor");
-const modalLogin = $(".modal-login")
-const cerrarLogin = $(".cerrar-login")
-
-abrirLogin.click(function () {
-    modalContainer.addClass("modal-active")
+botonAbrir.addEventListener("click", () => {
+  contenedorModal.classList.add("modal-active");
+});
+botonCerrar.addEventListener("click", () => {
+  contenedorModal.classList.remove("modal-active");
 });
 
-cerrarLogin.click(function () {
-    modalContainer.removeClass("modal-active")
+
+function Producto(nombre, precio, id, img) {
+  this.nombre = nombre;
+  this.precio = precio;
+  this.id = id;
+  this.img = img;
+}
+
+
+// check out
+
+// cuando toque el boton de check-out se abre un modal de checkout que cuando pone submit 
+//se borra todo carrito y pone un cartel de enviado
+
+const checkout = $(".checkout");
+const checkContainer = $("#checkout-container");
+
+$("#checkout").click(function () {
+  console.log("toco")
+  checkContainer.addClass("modal-active");
+  contenedorModal.classList.remove("modal-active");
+});
+
+$("#realizar-compra").click(function () {
+  checkContainer.removeClass("modal-active");
+
+});
+
+$("#cerrar-compra").click(function(){
+  checkContainer.removeClass("modal-active");
 })
-
-
-const inicioSesion = ("#inicio-sesion");
-
-class User {
-    constructor(nombre, email, pass) {
-        this.nombre = nombre;
-        this.email = email;
-        this.pass = pass;
-    }
-}
-function login() {
-    var nombre = $("#user").value;
-    var email = $("#email").value;
-    var pass = $("#pass").value;
-    let user1 = new User(nombre, email, pass);
-    sessionStorage.setItem("usuario", user1.nombre);
-}
-
-function agregarNombre() {
-    if (sessionStorage.getItem("usuario")) {
-        let usuarioActual = sessionStorage.getItem("usuario");
-
-        var loginContainer = $("#current-user");
-        loginContainer.append(`<div> ${usuarioActual} </div>`)
-        
-        modalContainer.removeClass("modal-active");
-    }
-}
-
-
